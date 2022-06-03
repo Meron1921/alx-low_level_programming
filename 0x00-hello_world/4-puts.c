37961f351d7dca90485a61dd30268a912552d10f
meron1921 <merondejene1921@gmail.com>
2022-06-03 04:17:38 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,22 @@
-#include<stdio.h>                                                              
-/**                                                                             * main - Entry point
- *
- * Return: Always 0 (Sucess)
- */
-int main(void)
-{
-	puts("\"Programming is like buliding a multilingual puzzle");
-	return (0);
-}
+#include<stdio.h>
 
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
+x
