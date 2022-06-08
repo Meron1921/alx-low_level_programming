ff86a6d3b833dd04d2e95aba34491eec85960eb7
meron1921 <merondejene1921@gmail.com>
2022-06-08 09:17:42 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
-#include "holberton.h"
-void print_alphabet_x10(void);
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-	int a, b;
+	int i;
+	char c;
 
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i < 10; i++)
 	{
-		for (b = 97; b <= 122; b++)
-		{
-			_putchar(b);
-		}
-	_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
 	}
 }
